83a88aa9bd4b847f4ef750d0a6c54c40df5e4ec6
vedant gupta <vedantgupta96@gmail.com>
2024-07-28 09:00:15 -0700

    Time: 562 ms (88.61%) | Memory: 20.7 MB (32.31%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,7 +3,7 @@ class Solution:
         ans = []
         nums.sort()
         for i in range(len(nums)):
-            if nums[i]>0:
+            if nums[i]>0:#the list is sorted so if 1st element is greater than 0 then there is no way to find a solution since all other values will also be greater than 0
                 break
             if i==0 or nums[i-1] != nums[i]:
                 self.twoSum(nums,i,ans)
@@ -16,7 +16,7 @@ class Solution:
                 end-=1
             elif sum < 0:
                 start += 1
-            else:
+            else:#don't forget to start+1 and end-1, also the while loop to increase start till we get a different value since duplicates are not allowed
                 ans.append([nums[i],nums[start],nums[end]])
                 start+=1
                 end-=1
