67f77381c9a447046c9fadb03274321a8e8cdcfe
vedant gupta <vedantgupta96@gmail.com>
2024-08-16 21:58:31 -0700

    Time: 3191 ms (85.68%) | Memory: 16.6 MB (59.10%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -2,11 +2,11 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows = len(board)
         cols = len(board[0])
-        visited = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
         def searchWord(r,c,i):
             if i == len(word):
                 return True
-            if 0>r or r>=len(board) or 0>c or c>=len(board[0]) or board[r][c] != word[i] or visited[r][c]:
+            if 0>r or r>=rows or 0>c or c>=cols or board[r][c] != word[i] or visited[r][c]:
                 return False
             visited[r][c] = True
             if searchWord(r+1,c,i+1) or searchWord(r-1,c,i+1) or searchWord(r,c+1,i+1) or searchWord(r,c-1,i+1):
