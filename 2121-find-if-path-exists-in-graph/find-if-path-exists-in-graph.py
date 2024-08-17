24dd634e9b7cf3b27f00fa02f7b5b9603c598f9c
vedant gupta <vedantgupta96@gmail.com>
2024-08-17 14:03:57 -0700

    Time: 2878 ms (46.94%) | Memory: 139.8 MB (25.09%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,5 +1,8 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if source == destination:
+            return True
+
         graph = defaultdict(list)
         seen = set()
         seen.add(source)
@@ -12,11 +15,11 @@ class Solution:
 
             if i == destination:
                 return True
+            seen.add(i)
             for nei_node in graph[i]:
-                if nei_node not in seen:
-                    seen.add(nei_node)
-                    if dfs(nei_node):
-                        return True
+                if nei_node not in seen and dfs(nei_node):
+                    return True
+                        
             return False
         return dfs(source)
 
