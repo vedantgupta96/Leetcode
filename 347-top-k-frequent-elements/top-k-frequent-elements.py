349e4817f5ef8f21fa0b1f123abda01ade3fe611
vedant gupta <vedantgupta96@gmail.com>
2024-08-23 11:03:04 -0700

    Time: 80 ms (92.46%) | Memory: 21.3 MB (44.27%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,11 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        c = Counter(nums)
-        c = [(-v,k) for k,v in c.items()]
-        heapq.heapify(c)
+        count = defaultdict(int)
+        for i in nums:
+            count[i] += 1
+        
+        count = [(-v,k) for (k,v) in count.items()]
+        heapq.heapify(count)
         output = []
         for i in range(k):
-            item = heapq.heappop(c)
+            item = heapq.heappop(count)
             output.append(item[1])
         return output
-        
+
