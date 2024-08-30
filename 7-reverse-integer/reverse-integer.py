b4de4290fc88d564421784889c7a8619cc8febdf
vedant gupta <vedantgupta96@gmail.com>
2024-08-30 11:55:43 -0700

    Time: 30 ms (89.64%) | Memory: 16.5 MB (80.16%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -8,9 +8,9 @@ class Solution:
             digit = int(math.fmod(x,10))
             x = int(x/10)
 
-            if(ans > MAX//10) or (digit > MAX%10 and ans == MAX//10):
+            if(ans > MAX//10) or (digit >= MAX%10 and ans == MAX//10):
                 return 0
-            if(ans < MIN//10) or (digit < MAX%10 and ans == MIN//10):
+            if(ans < MIN//10) or (digit <= MAX%10 and ans == MIN//10):
                 return 0
             ans = (ans*10)+digit
 
