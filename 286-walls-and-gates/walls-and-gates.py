6c18f8c1c2f199da6fec02fa1ef99e56eb627f9d
vedant gupta <vedantgupta96@gmail.com>
2024-09-01 14:47:30 -0700

    Time: 166 ms (94.57%) | Memory: 19.5 MB (60.04%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -6,19 +6,14 @@ class Solution:
         m, n = len(rooms), len(rooms[0])
         ROOM, GATE, WALL = 2**31-1,0,-1
         q = deque()
-
         for r in range(m):
             for c in range(n):
                 if rooms[r][c] == GATE:
                     q.append((r,c,0))
-        
         while q:
             x,y,d = q.popleft()
-            print(x,y,d)
-
             for i,j in [(1,0),(0,1),(-1,0),(0,-1)]:
                 r,c = x+i,y+j
-                print(r,c)
                 if 0<=r<m and 0<=c<n and rooms[r][c] == ROOM:
                     q.append((r,c,d+1))
                     rooms[r][c] = d+1
