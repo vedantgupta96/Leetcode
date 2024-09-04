63075860ff6a0aea7d484aff025026e6ae3a7b97
vedant gupta <vedantgupta96@gmail.com>
2024-09-04 11:19:08 -0700

    Time: 114 ms (47.76%) | Memory: 18.4 MB (81.93%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -15,3 +15,17 @@ class Solution:
                 rightMax = max(rightMax,height[r])
                 totalWater += rightMax - height[r]
         return totalWater
+
+        # ans = 0
+        # size = len(height)
+        # for i in range(1, size - 1):
+        #     left_max = 0
+        #     right_max = 0
+        #     # Search the left part for max bar size
+        #     for j in range(i, -1, -1):
+        #         left_max = max(left_max, height[j])
+        #     # Search the right part for max bar size
+        #     for j in range(i, size):
+        #         right_max = max(right_max, height[j])
+        #     ans += min(left_max, right_max) - height[i]
+        # return ans
