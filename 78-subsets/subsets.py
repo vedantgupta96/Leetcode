6f11c10e539a91ffd554cef420969ae44b2acbcb
vedant gupta <vedantgupta96@gmail.com>
2024-09-03 19:40:13 -0700

    Time: 34 ms (76.90%) | Memory: 16.7 MB (26.38%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,18 +1,32 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        stack = [(0, [])]  # Stack to hold (index, current subset)
+        stack = [(0,[])]
 
         while stack:
             index, subset = stack.pop()
 
-            # If we've processed all elements, add the subset to the result
             if index == len(nums):
                 ans.append(subset)
             else:
-                # Decision to include nums[index] in the subset
-                stack.append((index + 1, subset + [nums[index]]))
-                # Decision to exclude nums[index] from the subset
-                stack.append((index + 1, subset))
-
+                stack.append((index+1,subset+[nums[index]]))
+                stack.append((index+1,subset))
         return ans
+
+# ans = []
+#         subset = []
+
+#         def dfs(n):
+#             if n == len(nums):
+#                 ans.append(subset.copy())
+#                 return
+            
+#             subset.append(nums[n])
+#             dfs(n+1)
+#             subset.pop()
+#             dfs(n+1)
+#             return
+            
+            
+#         dfs(0)
+#         return ans
