bbcde52ec8c7a8ebc755c8279647c882fd08434a
vedant gupta <vedantgupta96@gmail.com>
2024-09-23 18:56:59 -0700

    Time: 2272 ms (35.76%) | Memory: 179 MB (13.71%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -7,24 +7,18 @@ class Solution:
                 manDist = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1])
                 distMap[i].append([manDist,j])
                 distMap[j].append([manDist,i])
-        
         ans = 0
         visit = set()
         minH = [[0,0]]
-
         while len(visit) < len(points):
             cost , i = heapq.heappop(minH)
-
             if i in visit:
                 continue
             visit.add(i)
             ans += cost
-
             for nCost,n in distMap[i]:
                 if n not in visit:
                     heapq.heappush(minH,[nCost,n])
-            
-
         return ans
 
 
